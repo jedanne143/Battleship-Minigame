ab793c2eb0fec40053729bf0d6631cff045c750f
Jedaiah Tantanella <jedanne143@gmail.com>
2024-08-17 07:10:34 -0400

    fixed styling and some bugs

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ let dancingAlien = document.getElementById('dancingAlien')
 //audio files
 const fighterSound = new Audio("audio/fighterF.mp3")
 const alienSound = new Audio("audio/alienF.mp3")
+const gameover = new Audio("audio/game-over-voice.mp3")
+const won = new Audio("audio/won.mp3")
+
 //Event listener to startgame
 startGame.addEventListener('click' , function () {
     //remove start button
@@ -40,6 +43,7 @@ startGame.addEventListener('click' , function () {
     // fire button and aliens appear
     battleStats.style.display ="flex"
     fireBtn.style.display = "flex";
+
     alien.forEach(alien => {
         alien.style.display = 'flex';
     });
@@ -72,22 +76,25 @@ startGame.addEventListener('click' , function () {
     fireBtn.addEventListener("click" , function (event) {
         //fighter attacks first
         fighterSound.play()
-        
-        if (alienShip.length === 0) {
-            alert('All aliens have been defeated. You won!');
-            fireBtn.style.display = 'none';
-            location.reload(); 
-        }
+
+
         if (fighter_.hull > 0){
             if (Math.random() < 0.7) {
                 alienShip[0].hull -= 5;
                 alert('You hit the target! Alien1 hull: ' + alienShip[0].hull);
+
                 if (alienShip[0].hull <= 0){
                     //removed destroyed alien in the array
 
-
-                    
                     alienShip.shift();
+                    alert('Alien destroyed! Number of aliens left:' + alienShip.length)
+                            //to determine if there's alien left
+                            if (alienShip.length === 0) {
+                                won.play();
+                                alert('All aliens have been defeated. You won!');
+                                fireBtn.style.display = 'none';
+                                location.reload(); 
+                            }
                     //update alienStat of new alien
                     alienHull.innerText = "Hull: " + alienShip[0].hull
                     alienStat.appendChild(alienHull)
@@ -96,17 +103,19 @@ startGame.addEventListener('click' , function () {
                     alienAccuracy.innerText ="Accuracy: " + alienShip[0].accuracy
                     alienStat.appendChild(alienAccuracy)
                     //ask user to play more
-                    let playMore = document.createElement('button')
                     fireBtn.style.display = "none"
+                    let playMore = document.createElement('button')
                     playMore.innerText = "Play More"
-                    playMore.style.color = "#f0eed0"
-                    playMore.style.border = "1px solid yellow"
+                    // playMore.style.backgroundColor = "red"
+                    playMore.style.border = "1px solid lime"
+                    playMore.className = "choose"
                     control.appendChild(playMore)
                     let retreat = document.createElement('button')
                     control.appendChild(retreat)
                     retreat.innerText = "Retreat"
-                    retreat.style.color = "pink"
-                    retreat.style.border= "1px solid red"
+                    // retreat.style.backgroundColor = "red"
+                    retreat.style.border= "1px solid lime"
+                    retreat.className = "choose"
                     playMore.onclick = () => {
                         control.removeChild(retreat)
                         control.removeChild(playMore)
@@ -116,21 +125,15 @@ startGame.addEventListener('click' , function () {
                         alert("Game over!")
                         location.reload(); 
                     }
-                    alert('Alien destroyed! Number of aliens left:' + alienShip.length)
-                            //to determine if there's alien left
-                            if (alienShip.length === 0) {
-                                alert('All aliens have been defeated. You won!');
-                                fireBtn.style.display = 'none';
-                                location.reload(); 
-                            }
                     
                 } else {
                 //alien survived the fighters attack. It fires back
-                alienAttack() //////////////////////
+                alienAttack() 
                 }
 
             } else {
                 alert("You missed! Alien hull:" + alienShip[0].hull);
+                //alien attacks
                 alienSound.play();
                 if (Math.random() < alienShip[0].accuracy){
                     fighter_.hull -= alienShip[0].firepower
@@ -142,6 +145,7 @@ startGame.addEventListener('click' , function () {
                 
             }
     } else {
+        gameover.play()
         alert("You lost! Aliens won")
         location.reload(); 
     }
@@ -154,6 +158,11 @@ function alienAttack () {
         fighter_.hull -= alienShip[0].firepower
         fighterHull.innerText = "Hull: " + fighter_.hull
         alert('Alien hit your ship: Fighter hull' + fighter_.hull )
+        if (fighter_.hull <= 0 ){
+            gameover.play()
+            alert("You lost! Aliens won")
+            location.reload(); 
+        }
     } else {
         alert('Alien shoot bad')
     }
